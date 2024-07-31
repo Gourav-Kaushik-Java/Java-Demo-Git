86757db41829530681c44e9646074787dd3e2e32
Gourav Kaushik <7gouravkaushik777@gmail.com>
2024-07-31 14:49:02 +0530

    Comparator example in java 8

---
+++ MyClass.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MyClass {
+    public static void main(String[] args){
+       Student s1 = new Student(2,"Vipul");
+       Student s2 = new Student(3,"Raj");
+       Student s3 = new Student(33,"Akshit");
+       List<Student> li = new ArrayList<>();
+       li.add(s1);
+       li.add(s2);
+       li.add(s3);
+       Collections.sort(li, (a,b) -> b.id - a.id);
+       System.out.println(li);
+    }
+
+    static class Student{
+        public Integer id;
+        public String name;
+        public Student(Integer id, String name){
+            this.id = id;
+            this.name = name;
+        }
+
+        public String toString(){
+            return this.id + " : " + this.name;
+        }
+    }
+} 
