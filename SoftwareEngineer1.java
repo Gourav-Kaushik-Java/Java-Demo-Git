847e60ea7fc6da3da0a8c86344bc38ef3253f3e3
Gourav Kaushik <7gouravkaushik777@gmail.com>
2024-07-31 15:33:06 +0530

    a class which implements interface and override its methods

---
+++ SoftwareEngineer1.java
@@ -0,0 +1,14 @@
+public class SoftwareEngineer1 implements MyInterface1 {
+
+    @Override
+    public String getSalary() {
+        return "10";
+    }
+
+    @Override
+    public String getDesignation() {
+        return "Software Engineer";
+    }
+    
+    
+}
